6a0869b9afc27acb12e311171b74bdf3bbd64e91
notrich22 <notrich22@mail.ru>
2024-11-16 13:18:11 +0300

    NO CYRILLIC: BASE HANDLING DONE

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #define variant "5.16"
 #define author "Aleksandr Vasilev"
-//#define DEBUG 
+#define DEBUG 
 struct Sentence{
     char* content;
     size_t length;
@@ -54,43 +54,64 @@ struct Text string_handling(char* s, size_t length){
     for (int j = 0; j < length; j++) {
         if (s[j] == '.') {
             // Пропуск пробела после точки
+            int offset = 0;
             if (s[j + 1] == ' ') {
-                j++;  // Сдвигаем индекс, чтобы пропустить пробел
+                offset = 1; // Указываем смещение, чтобы пропустить пробел
             }
+
+            // Создаем предложение
             text.sentences[i++] = create_sentence(start_pointer, j - (start_pointer - s) + 1);
-            start_pointer = s + j + 1;
+
+            // Обновляем start_pointer, пропуская точку и пробел (если он есть)
+            start_pointer = s + j + 1 + offset;
+
+            // Пропуск начальных пробелов перед следующим предложением
+            while (*start_pointer == ' ' && *start_pointer != '\0') {
+                start_pointer++;
+            }
         }
     }
 
-    // Удаление дубликатов
-    /*for (int i = 0; i < text.sentences_count; i++) {
-        for (int j = i + 1; j < text.sentences_count; j++) {
-            if (text.sentences[j].content != NULL && text.sentences[i].content != NULL) {
-                char *lower_i = lower(text.sentences[i]).content;
-                char *lower_j = lower(text.sentences[j]).content;
-                #ifdef DEBUG
-                printf("%s",lower_i);
-                printf("%s",lower_j);
-                #endif
-                if (strcmp(lower_i, lower_j) == 0) {
-                    free(text.sentences[j].content); // Освобождаем память
-                    text.sentences[j].content = NULL; // Помечаем как удалённую
-                }
-                free(lower_i);
-                free(lower_j);
-            }
-        }
-    }*/
-/*
-    // Перенос уникальных предложений в новый массив
-    int unique_count = 0;
+ // Удаление дубликатов
     for (int i = 0; i < text.sentences_count; i++) {
         if (text.sentences[i].content != NULL) {
-            text.sentences[unique_count++] = text.sentences[i];
+            for (int j = i + 1; j < text.sentences_count; j++) {
+                if (text.sentences[j].content != NULL) {
+                    char* lower_i = lower(text.sentences[i]).content;
+                    char* lower_j = lower(text.sentences[j]).content;
+                    #ifdef DEBUG
+                        printf("lower_i: '%s' (length: %zu)\n", lower_i, strlen(lower_i));
+                        printf("lower_j: '%s' (length: %zu)\n", lower_j, strlen(lower_j));
+                    #endif
+                    if (strcmp(lower_i, lower_j) == 0) {
+                        free(text.sentences[j].content); // Освобождаем память
+                        text.sentences[j].content = NULL; // Помечаем как удалённую
+                    }
+                    free(lower_i);
+                    free(lower_j);
+                }
+            }
         }
-    }*/
+    }
+    
+    // Перенос уникальных предложений в новый массив
+    int unique_count = 0;
+    struct Sentence* unique_sentences = (struct Sentence*) calloc(text.sentences_count, sizeof(struct Sentence));
+    for (int i = 0; i < text.sentences_count; i++) {
+        if (text.sentences[i].content != NULL) {
+            unique_sentences[unique_count++] = text.sentences[i];
+        }
+    }
+
+    // Освобождаем старый массив и обновляем указатель
+    free(text.sentences);
+    text.sentences = unique_sentences;
+    text.sentences_count = unique_count;
+
     return text;
 }
+
+
 struct Text get_text() {
     struct Text text;
 
@@ -100,7 +121,6 @@ struct Text get_text() {
     char *s = (char*) malloc(sizeof(char));
 
     char c = getchar();
-    c = getchar();
 
     while (c != '\n') {
         s[len++] = c;
@@ -123,6 +143,11 @@ void print_text(struct Text text) {
     }
 }
 
+
+void print_help(){
+    printf("1) Изменить все слова в тексте заканчивающиеся на символ цифры (0-9) так чтобы они заканчивались на название цифры на кириллице\n2) Вывести все предложения в которых встречается последнее слово предыдущего предложения.\n3) Отсортировать предложения по уменьшению количества слов в предложении.\n4) Удалить все предложения в которых два и меньше слова.\n5) Вызов справки\n");
+    return;
+}   
 
 int main(){
     setlocale(LC_ALL, ""); // Устанавливаем локаль для поддержки UTF-8
@@ -152,6 +177,9 @@ int main(){
             break;
         case 4:
             s = get_text();
+            break;
+        case 5:
+            print_help();
             break;
         default:
             printf("Error: Данные некорректны\n");
